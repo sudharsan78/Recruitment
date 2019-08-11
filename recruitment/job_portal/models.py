c69a368383713d2f3549f7717ab4edb6b4591954
sudharsan v.s <vssudharsan78@gmail.com>
2019-08-11 11:34:18 +0530

    Created Job Post model

--- recruitment/job_portal/models.py
+++ recruitment/job_portal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class JobPost(models.Model):
+    position = models.CharField(max_length=50)
+    country = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    experience = models.CharField(max_length=40)
+    primary_skill = models.CharField(max_length=120)
+    secondary_skill = models.CharField(max_length=120)
+    salary = models.CharField(max_length=60)
+    job_description = models.TextField()
+
+    def __str__(self):
+        return self.position
+
